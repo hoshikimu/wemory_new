668da28941b3352bf886fcfd3db6118c787ea6f0
vagrant <vagrant@localhost.localdomain>
2020-08-18 03:11:10 +0000

    add search and permission pages

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -6,6 +6,6 @@ class HomesController < ApplicationController
   def top
     @post_image = PostImage.find(1)
     @categories = Category.all
-    @post_images = PostImage.all.order("RANDOM()").limit(9)
+    @post_images = PostImage.where(user_id: current_user.id).order("RANDOM()").limit(9)
   end
 end
