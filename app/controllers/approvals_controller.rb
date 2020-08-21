c8632947470ccec62114ecc64960bbee76d8e94f
vagrant <vagrant@localhost.localdomain>
2020-08-21 11:40:40 +0000

    Add index_page for approvers and approvereds

--- app/controllers/approvals_controller.rb
+++ app/controllers/approvals_controller.rb
@@ -11,12 +11,18 @@ class ApprovalsController < ApplicationController
     new_approval = Approval.new
     new_approval.permission_status = params[:permission_status]
     new_approval.approver_id = current_user.id
-    new_approval.approvered_id = params[:user]
+    new_approval.approvered_id = params[:user_id]
     if new_approval.save!
       redirect_to top_path
     else
       render :search
     end
+  end
+
+  def index
+    @user_selected = params[:selected]
+    @approvers = Approval.where(approver_id: current_user.id)
+    @approvereds = Approval.where(approvered_id: current_user.id)
   end
 
   # private
--- app/models/approval.rb
+++ app/models/approval.rb
@@ -1,6 +1,6 @@
 class Approval < ApplicationRecord
-  belongs_to :approver, class_name: "User", optional: true
-  belongs_to :approvered, class_name: "User", optional: true
+  belongs_to :approver, class_name: "User"
+  belongs_to :approvered, class_name: "User"
 
-  enum permission_status: {権限なし: 1, 閲覧者: 2, 編集者: 3}
+  enum permission_status: {閲覧者: 0, 編集者: 1}
 end
