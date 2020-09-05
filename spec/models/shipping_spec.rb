4ad081888c2c7fcfc18a7d36320abc1dea6787ac
vagrant <vagrant@localhost.localdomain>
2020-09-05 10:55:44 +0000

    Add test for user_model and shipping_model

--- spec/models/shipping_spec.rb
+++ spec/models/shipping_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Shipping, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'バリデーションのテスト' do
+    let(:shipping) { create(:shipping) }
+    subject { shipping.valid? }
+    context 'postal_codeカラム' do
+      it '空欄でないこと' do
+        shipping.postal_code = ''
+        is_expected.to eq false
+      end
+    end
+  end
 end
